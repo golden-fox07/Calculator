86e8b4aaba5b594dc9b5fe60b688413bbbd30ab5
Aakriti Agarwal <aakriti07agarwal@gmail.com>
2025-05-04 18:38:59 +0530

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,23 @@
+window.onload = function () {
+    const display = document.getElementById('display');
+    const buttons = document.querySelectorAll('input[type="button"]');
+  
+    buttons.forEach(button => {
+      const value = button.value;
+  
+      button.addEventListener('click', () => {
+        if (button.dataset.action === "clear") {
+          display.value = '';
+        } else if (value === "=") {
+          try {
+            display.value = eval(display.value);
+          } catch (e) {
+            display.value = 'Error';
+          }
+        } else {
+          display.value += button.dataset.value || value;
+        }
+      });
+    });
+  };
+  
